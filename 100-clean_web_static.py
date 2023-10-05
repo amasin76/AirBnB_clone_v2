be33194cacbe9c9991fc1acb9f379d828f62692b
amasin76 <amasin76@gmail.com>
2023-10-05 16:23:30 +0000

    Keep it clean!

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -15,10 +15,11 @@ def do_clean(number=0):
     number (int): The number of archives to keep.
     """
 
-    number = int(number)
-    if number < 2:
+    if number == 0:
         number = 2
+    else:
+        number += 1
 
-    local("ls -t versions | tail -n +{} | xargs rm -f --".format(number))
-    run("ls -t /data/web_static/releases | tail -n +{} | xargs rm -rf --"
-        .format(number))
+    local('cd versions && ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    path = '/data/web_static/releases'
+    run('cd {} && ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
