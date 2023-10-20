f73cb73c5265e69e88e6d362751052e423c07626
amasin76 <amasin76@gmail.com>
2023-10-20 20:26:32 +0000

    Fix: typo

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,7 +10,7 @@ def hello_hbnb():
     return 'Hello HBNB!'
 
 
-@app.route('/hbtn', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Displays greeting"""
     return 'HBNB'
